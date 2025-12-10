8f0ebd2adf8a986f7ca78cc65b49bc71c2df3fc8
TQY <17823167416@163.com>
2025-12-10 17:18:18 +0800

    纠正配置文件中的命名错误

--- src/config.ts
+++ src/config.ts
@@ -6,8 +6,8 @@ export  const LANGUAGE_MAP: Record<string, string> = {
   'it': '意大利语',
   'nl': '荷兰语',
   'pl': '波兰语',
-  'se': '瑞典语',
-  'dk': '丹麦语',
+  'sv': '瑞典语',
+  'da': '丹麦语',
   'cz': '捷克语',
   'be': '白俄罗斯语'
 };
